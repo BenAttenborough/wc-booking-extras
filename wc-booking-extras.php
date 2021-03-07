04f073caa470a9836bdc146a4cc45ab88eb6c04c
Ben Attenborough <battenborough@gmail.com>
2021-03-07 19:59:22 +0000

    Initial files

---
+++ wc-booking-extras.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wc Booking Extras
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wc-booking-extras
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wc_Booking_Extras
+ */
+
+// Your code starts here.
